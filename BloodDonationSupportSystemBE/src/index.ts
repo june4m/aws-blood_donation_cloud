e9fb00f298873f93f38480eb5551366ac1839792
june4m <congminh04062004@gmail.com>
2025-12-11 15:10:26 +0700

    change CORS

--- BloodDonationSupportSystemBE/src/index.ts
+++ BloodDonationSupportSystemBE/src/index.ts
@@ -13,13 +13,17 @@ const port = 3000
 // Middleware to parse JSON
 // app.use(cors())
 app.use(
-		cors({
-			origin: 'https://main.d1ag7fwsuhqivq.amplifyapp.com/',
-			credentials: true,
-			methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-			allowedHeaders: ['Content-Type', 'Authorization']
-		})
-	)
+	cors({
+		origin: [
+			'https://main.d1ag7fwsuhqivq.amplifyapp.com',
+			'https://main.d1fnooytkqycdg.amplifyapp.com',
+			'http://localhost:5173'
+		],
+		credentials: true,
+		methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+		allowedHeaders: ['Content-Type', 'Authorization']
+	})
+)
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ limit: '10mb', extended: true }))
 app.use(cookieParser())
@@ -32,5 +36,5 @@ const scheduleCronJobController = new ScheduleCronJobController()
 scheduleCronJobController.init()
 // Start server
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
+	console.log(`Server running on http://localhost:${port}`)
 })
